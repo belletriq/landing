8e2a9e47d3f6103bf5ff84c5f1b02f96df9ff579
makspooh <maksimpooh@gmail.com>
2023-09-15 13:57:29 +0300

    scss mixins, variables

--- src/components/layouts/Main/index.js
+++ src/components/layouts/Main/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 
+import styles from "./index.module.scss";
+
 function MainLayout() {
-  return <div />;
+  return (
+    <div className={styles.container}>
+      <div className={styles.content} />
+    </div>
+  );
 }
 
 export default MainLayout;
